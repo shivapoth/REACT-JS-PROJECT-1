b55e1fee7f931dcc2d1604c6d9ce9a779f18c95f
shivapoth <shivateja.pothapragada@ojas-it.com>
2023-12-07 18:42:11 +0530

    Started Working on App Bar

--- Single-Sign-On/Frontend/src/App.js
+++ Single-Sign-On/Frontend/src/App.js
@@ -2,17 +2,20 @@ import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
 import Register from './components/Register/Register';
+import Navbar from './components/NavBar/Navbar';
 
 function App() {
   return (
     <div className="App">
-      {/* <Router>
+      <Router>
         <Routes>
-          <Route path='/' element={<Login />} />
+          <Route path='/' element={<Navbar />} />
           <Route path='/register' element={<Register />} />
+          <Route path='/home' element={<Navbar />} />
         </Routes>
-      </Router> */}
-      <Register/>
+      </Router>
+      
+      {/* <Navbar/> */}
     </div>
   );
 }
