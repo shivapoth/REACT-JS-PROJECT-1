b0767ffffa5c8ff1b9ee39fe892618608874a2e5
Sai Prasanna Dupati <648fcba0a14a5814d62207c2@1able.ai>
2023-12-11 11:02:47 +0530

    git conflict resolved

--- Single-Sign-On/Frontend/src/App.js
+++ Single-Sign-On/Frontend/src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Login from './components/Login/Login';
 import Register from './components/Register/Register';
 import FooterPage from './components/Home/Footer';
+import Navbar from './components/NavBar/Navbar';
 
 function App() {
   return (
