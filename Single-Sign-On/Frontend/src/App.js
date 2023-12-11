10c97dcfa13058a056dec0b0c45ceacfc64ad350
shivapoth <shivateja.pothapragada@ojas-it.com>
2023-12-11 11:00:33 +0530

    Created Home Component

--- Single-Sign-On/Frontend/src/App.js
+++ Single-Sign-On/Frontend/src/App.js
@@ -1,15 +1,16 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import './App.css';
 import Login from './components/Login/Login';
-import Register from './components/Register/Register';
-import FooterPage from './components/Home/Footer';
+import Register from './components/Registration/Register';
+import FooterPage from './components/Footer/Footer';
+import Navbar from './components/NavBar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-          <Route path='/' element={<Navbar />} />
+          <Route path='/' element={<Navbar/>} />
           <Route path='/register' element={<Register />} />
           <Route path='/home' element={<Navbar />} />
         </Routes>
