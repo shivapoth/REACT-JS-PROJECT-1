29903ea6562a01148175bd32796967574480f063
shivapoth <shivateja.pothapragada@ojas-it.com>
2023-12-12 18:37:54 +0530

    Started Second part of Details Component

--- Single-Sign-On/Frontend/src/components/Home/OurCause/OurCause.jsx
+++ Single-Sign-On/Frontend/src/components/Home/OurCause/OurCause.jsx
@@ -1,29 +1,25 @@
-import React, { useEffect } from "react";
+import React from "react";
+import { Button, Card, Col, Container, Row } from "react-bootstrap";
 import "./ourcause.css";
 import sliderImage from "../../assests/images/sliderimage.png";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 import $ from "jquery";
 import "slick-carousel";
-const OurCause = () => {
-  // useEffect(() => {
-  //   $(document).ready(function () {
-  //     $(".card").slick();
-  //   });
-  // }, []);
 
+const OurCause = () => {
   return (
-    <div className="container">
-      <div className="row">
-        <div className="col-lg-6 col-md-6 col-sm-12">
-          <div className="">
+    <Container>
+      <Row>
+        <Col lg={6} md={6} sm={12}>
+          <div>
             <span className="text-emergency">
               We will create a UK without the need for emergency food
             </span>
             <p className="text-content">
-              We support a nationwide network of food banks and together we
-              provide emergency food and support to people locked in poverty,
-              and campaign for change to end the need for food banks in the UK.
+              We support a nationwide network of food banks, and together we
+              provide emergency food and support to people locked in poverty and
+              campaign for change to end the need for food banks in the UK.
             </p>
             <div>
               <a
@@ -34,7 +30,7 @@ const OurCause = () => {
                 <svg
                   viewBox="0 0 24 24"
                   xmlns="http://www.w3.org/2000/svg"
-                  class="arrow"
+                  className="arrow"
                 >
                   <path fill="none" d="M0 0h24v24H0z"></path>
                   <path d="M12 4l-1.41 1.41L16.17 11H4v2h12.17l-5.58 5.59L12 20l8-8z"></path>
@@ -42,12 +38,22 @@ const OurCause = () => {
               </a>
             </div>
           </div>
-        </div>
-        <div className="col-lg-6 col-md-6 col-sm-12">
-          <div className="card"></div>
-        </div>
-      </div>
-    </div>
+        </Col>
+        <Col lg={6} md={6} sm={12}>
+          <Card style={{ width: "18rem" }}>
+            <Card.Img variant="top" src={sliderImage} alt="Logo Here" />
+            <Card.Body>
+              <Card.Title>Card Title</Card.Title>
+              <Card.Text>
+                Some quick example text to build on the card title and make up
+                the bulk of the card's content.
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </Card>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
